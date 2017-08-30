28aa7ed2980874563c74fbdc53cd76ed4e4d8c8b
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-08-30 12:08:27 -0700

    Update bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,27 +1,47 @@
-var bubble = function(arr){
-    var left = 0;
-    var right = 1;
-    var oldArr = [...arr];
-    var newArr = [];
+// var bubble = function(arr){
+//     var left = 0;
+//     var right = 1;
+//     var oldArr = [...arr];
+//     var newArr = [];
 
-    if(oldArr.length === 0) return oldArr;
+//     if(oldArr.length === 0) return oldArr;
 
-    while(oldArr.length >= 1){
-        for(var i = 0; i < oldArr.length; i++){
-            if(oldArr[left] > oldArr[right]){
-                var temp = oldArr[left];
-                oldArr[left] = oldArr[right];
-                oldArr[right] = temp;
+//     while(oldArr.length >= 1){
+//         for(var i = 0; i < oldArr.length; i++){
+//             if(oldArr[left] > oldArr[right]){
+//                 var temp = oldArr[left];
+//                 oldArr[left] = oldArr[right];
+//                 oldArr[right] = temp;
                 
+//             }
+//             left++;
+//             right++;
+//         }
+//         left = 0;
+//         right = 1;
+//         newArr.unshift(oldArr.pop());
+//     }
+//     return newArr;
+// }
+
+var bubble = function(arr){
+    var newArr = [...arr];
+    var swapped = true;
+
+    while(swapped){
+        swapped = false;
+        for(var i = 0; i < newArr.length; i++){
+            if(newArr[i] > newArr[i+1]){
+                var temp = newArr[i];
+                newArr[i] = newArr[i+1];
+                newArr[i+1] = temp;
+                swapped = true;
             }
-            left++;
-            right++;
         }
-        left = 0;
-        right = 1;
-        newArr.unshift(oldArr.pop());
     }
+
     return newArr;
 }
 
 console.log(bubble([1,6,2,4,3,8,9]))
+console.log(bubble([3,2,1]))
