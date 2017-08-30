2085533901f1cd3901b7a635185d09bc32344a17
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-08-30 10:15:36 -0700

    Completed implementation of recursive sumDigits

---
+++ recursion/sumDigits.js
@@ -0,0 +1,15 @@
+/* 
+Write an algorithm called sumDigits that takes a 
+non-negative integer as input and returns the sum of 
+the integers. Do this exercise recursively. 
+*/
+
+var sumDigits = function(num, sum){
+    if(num === 0) return sum;
+    sum += num % 10;
+    newNum = Math.floor(num/10);
+    return sumDigits(newNum, sum)
+}
+
+
+console.log(sumDigits(678, 0))
