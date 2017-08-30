99156c93ea14ce46a06aef8afeed7e6cb15bca4c
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-08-30 10:21:15 -0700

    Practice recursive exponents

--- recursionIntro/exponents.js
+++ recursionIntro/exponents.js
@@ -1,23 +1,31 @@
-function exponent(base, expo){
-    var count = 2; 
-    var answer = base;
-    if(expo === 0) return 1;
+// function exponent(base, expo){
+//     var count = 2; 
+//     var answer = base;
+//     if(expo === 0) return 1;
+//     if(expo === 1) return base;
+//     while(count <= expo ){
+//         answer *= base;
+//         count++;
+//     }
+//     return answer;
+// }
+
+// console.log("*** Exponent with while loop ***");
+// console.log(exponent(2, 8));
+
+// function recursiveExponent(base, expo){
+//     if(expo === 0) return 1;
+//     if(expo === 1) return base;
+//     return recursiveExponent(base, expo-1);
+// }
+
+// console.log("*** Exponent with recursion ***");
+// console.log(exponent(2, 8));
+
+// 08/30/17
+var pow = function(base, expo){
     if(expo === 1) return base;
-    while(count <= expo ){
-        answer *= base;
-        count++;
-    }
-    return answer;
+    return pow(base, expo - 1) * base;
 }
 
-console.log("*** Exponent with while loop ***");
-console.log(exponent(2, 8));
-
-function recursiveExponent(base, expo){
-    if(expo === 0) return 1;
-    if(expo === 1) return base;
-    return recursiveExponent(base, expo-1);
-}
-
-console.log("*** Exponent with recursion ***");
-console.log(exponent(2, 8));
+console.log(pow(2,8))
